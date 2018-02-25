110598f28a57025d5986e87b32b2c6e50d1662db
Alexandre Kahn <alexandrekahn139@gmail.com>
2018-02-25 12:51:07 +0100

    Added React Module

---
+++ android/app/src/main/java/com/wifidirectpackage/WiFiDirectModule.java
@@ -0,0 +1,18 @@
+package com.wifidirectpackage;
+
+import com.facebook.react.bridge.ReactContextBaseJavaModule;
+
+/**
+ * Created by alexandre on 25/02/2018.
+ */
+
+public class WiFiDirectModule extends ReactContextBaseJavaModule {
+    @Override
+    public String getName() {
+        return "WiFiDirectModule";
+    }
+
+    
+
+
+}
