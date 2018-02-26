3f9d82ff36ba958697b536b8ad1b117379894f6f
Alexandre Kahn <alexandrekahn139@gmail.com>
2018-02-26 12:23:19 +0100

    Fixed the module net getting loaded in RN

--- android/app/src/main/java/com/wifidirectpackage/WiFiDirectModuleReactPackage.java
+++ android/app/src/main/java/com/wifidirectpackage/WiFiDirectModuleReactPackage.java
@@ -6,6 +6,7 @@ import com.facebook.react.bridge.ReactApplicationContext;
 import com.facebook.react.uimanager.ViewManager;
 
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 
 /**
@@ -22,6 +23,6 @@ public class WiFiDirectModuleReactPackage implements ReactPackage{
 
     @Override
     public List<ViewManager> createViewManagers(ReactApplicationContext reactContext) {
-        return null;
+         return Collections.emptyList();
     }
 }
